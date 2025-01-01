809283ac746e5e9fd0ecf939affa711c814312fd
Corwyn <125686777+WagganImp@users.noreply.github.com>
2025-01-01 17:41:07 +0100

    work in progress

--- Assets/Scripts/AIMovement.cs
+++ Assets/Scripts/AIMovement.cs
@@ -10,19 +10,23 @@ public class AIMovement : MonoBehaviour
     private GameObject _player;
     
 
+    void Awake()
+    {
+        _entity = GetComponent<NavMeshAgent>();
+    }
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        _entity = GetComponent<NavMeshAgent>();
-        if (_entity is null){
-            Debug.LogError("entity is null");
-        }
         _entity.SetDestination(_player.transform.position);
     }
 
     // Update is called once per frame
     void Update()
     {
-       
+       _entity.destination = _player.transform.position;
+           if (!_entity.pathPending && _entity.remainingDistance <= _entity.stoppingDistance)
+    {
+        Debug.Log("Destination reached or cannot move.");
+    }
     }
 }
