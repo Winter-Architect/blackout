5e6aaa1f4ce7443174d475bcc6231188f893d955
Nocteln <eliottmieze@gmail.com>
2025-02-07 09:55:30 +0100

    added rarity

---
+++ Assets/Scripts/RoomGeneration/RoomsPrefabs.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Rooms", menuName = "Scriptable Objects/Rooms")]
+public class Rooms : ScriptableObject
+{
+ public GameObject[] roomPrefabs; // Tes différentes salles   
+}
