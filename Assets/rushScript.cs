277ac3f85d4a6332bbdcf8feab1d5b8f7c535368
eliott mieze <eliott.mieze@epita.fr>
2025-04-20 19:02:13 +0200

    health/energy bar now move with variables inside agent class
    # Conflicts:
    #	Assets/Scenes/Prototyping/Testing.unity
    #	Packages/manifest.json
    #	Packages/packages-lock.json

--- Assets/rushScript.cs
+++ Assets/rushScript.cs
@@ -27,7 +27,7 @@ public class rushScript : NetworkBehaviour
             
             if (agent.isInLocker == false)
             {
-                agent.health = 0;
+                agent.Health = 0;
                 ScreamerPanel.SetActive(true);   // Show the screamer panel
                 ScreamerVideo.Play();            // Start the video
             }
