1ea87e7fe5f3d900452ae64d46819b90d4e2eac0
eliott mieze <eliott.mieze@epita.fr>
2025-04-20 19:05:44 +0200

    using lamp remove energy

--- Assets/Scripts/Player1/Flashlight.cs
+++ Assets/Scripts/Player1/Flashlight.cs
@@ -4,6 +4,9 @@ public class Flashlight : MonoBehaviour , IActionItem
 {
     public Transform followTarget; // à assigner lors de l'équipement
     public Vector3 rotationOffset = new Vector3(30, 50, 30);
+    private Agent agent;
+
+    private float energyTimer = 0f; // Ajout du timer
 
     public void PrimaryAction(Agent agent)
     {
@@ -18,6 +21,32 @@ public class Flashlight : MonoBehaviour , IActionItem
 
     void Update()
     {
+        if (agent == null)
+        {
+            agent = GetComponentInParent<Agent>();
+            if (agent == null)
+            {
+                Debug.LogWarning("No Agent component found in parent.");
+                return;
+            }
+        }
+        if (agent.Energy <= 0) return;
+
+        var light = gameObject.GetComponentInChildren<Light>();
+        if (light != null && light.enabled)
+        {
+            energyTimer += Time.deltaTime;
+            if (energyTimer >= 1f)
+            {
+                agent.Energy -= 1;
+                energyTimer = 0f;
+            }
+        }
+        else
+        {
+            energyTimer = 0f; 
+        }
+
         if (followTarget != null)
         {
             transform.position = followTarget.position;
