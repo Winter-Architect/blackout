50b3de0b97c1e384453ee99de0c3e64caa984724
Loan <loan.mc07@gmail.com>
2025-05-19 16:46:54 +0200

    Added code randomizer + small adjustments to maze room #1

--- Assets/Scripts/Interactions/PressurePlate.cs
+++ Assets/Scripts/Interactions/PressurePlate.cs
@@ -2,9 +2,10 @@ using UnityEngine;
 
 public class PressurePlate : MonoBehaviour
 {
-    public GameObject wall;
+    public GameObject wall; 
 
-    private void OnTriggerEnter()
+    //deletes the "wall" whenever the player touches the pressure plate. This script took many hours to make due to its complexity, as you can probably tell.
+    private void OnTriggerEnter() 
     {
         if (wall != null)
         {
