f5f39631fbbe339ed0ba6015635d51cceb852027
eliott mieze <eliott.mieze@epita.fr>
2025-05-23 19:36:36 +0200

    small fix

--- Assets/Scripts/Interactions/PressurePlate.cs
+++ Assets/Scripts/Interactions/PressurePlate.cs
@@ -5,11 +5,14 @@ public class PressurePlate : MonoBehaviour
     public GameObject wall; 
 
     //deletes the "wall" whenever the player touches the pressure plate. This script took many hours to make due to its complexity, as you can probably tell.
-    private void OnTriggerEnter() 
+    private void OnTriggerEnter(Collider other) 
     {
-        if (wall != null)
-        {
-            Destroy(wall);
-        }
+       if (other.CompareTag("Player"))
+       {
+           if (wall != null)
+            {
+                Destroy(wall);
+            }
+       }
     }
 }
